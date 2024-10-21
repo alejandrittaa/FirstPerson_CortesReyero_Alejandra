130a351d52421834dd65945fcc000fc4a2a4e965
alejandrittaa <alejandracortesreyero@gmail.com>
2024-10-21 20:13:08 +0200

    movimiento persoanje

--- Assets/Scripts/ThirdPerson.cs
+++ Assets/Scripts/ThirdPerson.cs
@@ -31,20 +31,19 @@ public class ThirdPerson : MonoBehaviour
 
         Vector2 input = new Vector3(h, v).normalized;
 
-        //creamos esto para que los ojos del jugador cuadren con el movimiento del mismo, ya que si gira la cabeza, tendra que moverse hacia delante y demás, en base a la rotación de su cabeza
-        //giramos sobre la Y, que es el palo que atraviesa al jugador por en medio
-        float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
-        
-        //suavizar el movimiento del personaje
-        float anguloSuave = Mathf.SmoothDampAngle(transform.eulerAngles.y, anguloRotacion, ref velocidadRotacion, suavizado);
-
-        //para que mi cuerpo quede orientado hacia donde me muevo
-        transform.eulerAngles = new Vector3(0, anguloSuave, 0);
-
 
         //si la magnitud/tamaño del vector es mayor de 0, si es positiva. 
         if (input.magnitude > 0)
         {
+            //creamos esto para que los ojos del jugador cuadren con el movimiento del mismo, ya que si gira la cabeza, tendra que moverse hacia delante y demás, en base a la rotación de su cabeza
+            //giramos sobre la Y, que es el palo que atraviesa al jugador por en medio
+            float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+
+            //suavizar el movimiento del personaje
+            float anguloSuave = Mathf.SmoothDampAngle(transform.eulerAngles.y, anguloRotacion, ref velocidadRotacion, suavizado);
+
+            //para que mi cuerpo quede orientado hacia donde me muevo
+            transform.eulerAngles = new Vector3(0, anguloSuave, 0);
 
             //mi movimiento queda rotado en base del ángulo calculado (un cuaternion indica una rotacion, mientras que un vector indica una posición)
             //tu frontal pasa a ser el ángulo que tenga la cámara

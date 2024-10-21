7ebaaab8e16e901fbf1cddbe8236265a4b86417e
alejandrittaa <alejandracortesreyero@gmail.com>
2024-10-21 19:44:01 +0200

    clase de hoy

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -25,16 +25,24 @@ public class FirstPerson : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal"); 
         float v = Input.GetAxisRaw("Vertical");
-        Vector3 movimiento = new Vector3 (h, 0 ,v).normalized;
+
+        Vector2 input = new Vector3 (h, v).normalized;
 
         //creamos esto para que los ojos del jugador cuadren con el movimiento del mismo, ya que si gira la cabeza, tendra que moverse hacia delante y demás, en base a la rotación de su cabeza
         //giramos sobre la Y, que es el palo que atraviesa al jugador por en medio
-        float anguloRotacion = Mathf.Atan2(movimiento.x, movimiento.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+        float anguloRotacion = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
 
-        if(movimiento.magnitude > 0)
+        //para que mi cuerpo quede orientado hacia donde me muevo
+        transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+
+
+        //si la magnitud/tamaño del vector es mayor de 0, si es positiva. 
+        if (input.magnitude > 0)
         {
-            //para que mi cuerpo quede orientado hacia donde me muevo
-            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+
+            //mi movimiento queda rotado en base del ángulo calculado (un cuaternion indica una rotacion, mientras que un vector indica una posición)
+            //tu frontal pasa a ser el ángulo que tenga la cámara
+            Vector3 movimiento = Quaternion.Euler(0, anguloRotacion, 0) * Vector3.forward; 
 
             //como no va por físicas, tendremos que multiplicarlo por Time.deltaTime
             characterController.Move(movimiento * velocidadMovimiento * Time.deltaTime);
